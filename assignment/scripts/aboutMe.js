302e2dc707007f3518e89b4816a4915c4addac68
Sharifa Abdirizak <sharifamabdirizak@gmail.com>
2023-07-23 15:09:35 -0500

    updated aboutMe.js and formatted vscode

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -36,15 +36,11 @@ if (adventurous == true){
 } else {
     console.log("How about we stay home?");
 }
-    
-
-
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
 if (luckyNumber ==2 && adventurous ==true);
 console.log("Roll the dice!");
-
 
 // 15 - Write a conditional that console logs "I can have more pets!" 
 //if the value of `pets` is less than the value of `allowedPets`,
